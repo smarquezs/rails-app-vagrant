88029ce1b54e2d9f9e8ad79e07ee179df06c5a7f
Sergio Márquez <smarquezs@gmail.com>
2014-11-08 14:57:40 -0300

    Init commit; ADD: chef folder

---
+++ chef/metadata.rb
@@ -0,0 +1,8 @@
+name             'chef'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures chef'
+long_description 'Installs/Configures chef'
+version          '0.1.0'
+
